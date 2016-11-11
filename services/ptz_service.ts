e3e2d60df24a94b73e917d9ef006da193b8c78e9
Manish Rastogi <manish.rastogi94@gmail.com>
2016-11-11 15:33:21 +0530

    Added few PTZ methods

--- services/ptz_service.ts
+++ services/ptz_service.ts
@@ -63,7 +63,7 @@ class PTZService extends SoapService {
         ContinuousZoomVelocitySpace : [{ 
           URI : 'http://www.onvif.org/ver10/tptz/ZoomSpaces/VelocityGenericSpace',
           XRange : { 
-            Min : -1.0,
+            Min : 0.33,
             Max : 1.0
           }
         }],
@@ -76,6 +76,167 @@ class PTZService extends SoapService {
       'AUX7on','AUX7off','AUX8on','AUX8off']
     }
 
+    var config = { 
+            attributes : {
+              token : 'ptz_config_token_1'
+            },
+
+            Name : 'PTZConfig_1',
+            UseCount : 0,
+            NodeToken : 'ptz_node_token_0',
+            // DefaultAbsolutePantTiltPositionSpace : '',
+            // DefaultAbsoluteZoomPositionSpace : '',
+            // DefaultRelativePanTiltTranslationSpace : '',
+            // DefaultRelativeZoomTranslationSpace : '',
+            DefaultContinuousPanTiltVelocitySpace : 'http://www.onvif.org/ver10/tptz/PanTiltSpaces/VelocityGenericSpace',
+            DefaultContinuousZoomVelocitySpace : 'http://www.onvif.org/ver10/tptz/ZoomSpaces/VelocityGenericSpace',
+            DefaultPTZSpeed : { 
+              PanTilt : { 
+                attributes : {
+                  x : 0.05,
+                  y : 0.05,
+                  space : 'http://www.onvif.org/ver10/tptz/PanTiltSpaces/VelocityGenericSpace'
+                }
+              },
+              Zoom : { 
+                attributes : {
+                  x : 0.07,
+                  space : 'http://www.onvif.org/ver10/tptz/ZoomSpaces/VelocityGenericSpace'
+                }
+              }
+            },
+            DefaultPTZTimeout : 'P0Y0M0DT1H',
+            PanTiltLimits : { 
+              Range : { 
+                URI : '',
+                XRange : { 
+                  Min : -1.0,
+                  Max : 1.0
+                },
+                YRange : { 
+                  Min : -1.0,
+                  Max : 1.0
+                }
+              }
+            },
+            ZoomLimits : { 
+              Range : { 
+                URI : '',
+                XRange : { 
+                  Min : 0.33,
+                  Max : 1
+                }
+              }
+            },
+            Extension : { 
+              PTControlDirection : { 
+                EFlip : { 
+                  Mode : 'OFF'
+                },
+                Reverse : { 
+                  Mode : 'OFF'
+                },
+                Extension : { }
+              },
+              Extension : { }
+            }
+        }
+
+      // var configOptions = { 
+      //     PTZConfigurationOptions : { 
+      //       attributes : {
+      //         PTZRamps : {tt:IntAttrList}
+      //       },
+      //       Spaces : { 
+      //         AbsolutePanTiltPositionSpace : [{ 
+      //           URI : { xs:anyURI},
+      //           XRange : { 
+      //             Min : { xs:float},
+      //             Max : { xs:float}
+      //           },
+      //           YRange : { 
+      //             Min : { xs:float},
+      //             Max : { xs:float}
+      //           }
+      //         }],
+      //         AbsoluteZoomPositionSpace : [{ 
+      //           URI : { xs:anyURI},
+      //           XRange : { 
+      //             Min : { xs:float},
+      //             Max : { xs:float}
+      //           }
+      //         }],
+      //         RelativePanTiltTranslationSpace : [{ 
+      //           URI : { xs:anyURI},
+      //           XRange : { 
+      //             Min : { xs:float},
+      //             Max : { xs:float}
+      //           },
+      //           YRange : { 
+      //             Min : { xs:float},
+      //             Max : { xs:float}
+      //           }
+      //         }],
+      //         RelativeZoomTranslationSpace : [{ 
+      //           URI : { xs:anyURI},
+      //           XRange : { 
+      //             Min : { xs:float},
+      //             Max : { xs:float}
+      //           }
+      //         }],
+      //         ContinuousPanTiltVelocitySpace : [{ 
+      //           URI : { xs:anyURI},
+      //           XRange : { 
+      //             Min : { xs:float},
+      //             Max : { xs:float}
+      //           },
+      //           YRange : { 
+      //             Min : { xs:float},
+      //             Max : { xs:float}
+      //           }
+      //         }],
+      //         ContinuousZoomVelocitySpace : [{ 
+      //           URI : { xs:anyURI},
+      //           XRange : { 
+      //             Min : { xs:float},
+      //             Max : { xs:float}
+      //           }
+      //         }],
+      //         PanTiltSpeedSpace : [{ 
+      //           URI : { xs:anyURI},
+      //           XRange : { 
+      //             Min : { xs:float},
+      //             Max : { xs:float}
+      //           }
+      //         }],
+      //         ZoomSpeedSpace : [{ 
+      //           URI : { xs:anyURI},
+      //           XRange : { 
+      //             Min : { xs:float},
+      //             Max : { xs:float}
+      //           }
+      //         }],
+      //         Extension : { }
+      //       },
+      //       PTZTimeout : { 
+      //         Min : { xs:duration},
+      //         Max : { xs:duration}
+      //       },
+      //       PTControlDirection : { 
+      //         EFlip : { 
+      //           Mode : [{ xs:string}],
+      //           Extension : { }
+      //         },
+      //         Reverse : { 
+      //           Mode : [{ xs:string}],
+      //           Extension : { }
+      //         },
+      //         Extension : { }
+      //       },
+      //       Extension : { }
+      //     }      
+      //   }
+
     port.GetNode = (args) => {
       var GetNodeResponse = { PTZNode: node };
       return GetNodeResponse;
@@ -85,6 +246,18 @@ class PTZService extends SoapService {
       var GetNodesResponse = { PTZNode: node };
       return GetNodesResponse;
     };
+
+    port.GetConfigurations = (args) => {
+      var GetConfigurationsResponse = { PTZConfiguration : config };
+      return GetConfigurationsResponse;
+    };
+
+    port.GetConfiguration = (args) => {
+      var GetConfigurationResponse = { PTZConfiguration : config };
+      return GetConfigurationResponse;
+    };
+
+
 
     port.SetHomePosition = (args) => {
       if (this.callback) this.callback('sethome', {});

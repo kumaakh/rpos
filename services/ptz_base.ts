d685810da7fd9fd5ba4bc741f17bcae8d2ea98d2
radbrawler <radbrawler@gmail.com>
2016-11-15 14:06:59 +0530

    status() fixes in baseptz

--- services/ptz_base.ts
+++ services/ptz_base.ts
@@ -244,34 +244,6 @@ class PTZBase {
       }
     };
 
-    this.ptzstatus = {
-      PTZStatus: {
-        Position: {
-          PanTilt: {
-            attributes: {
-              x: { xs: float },
-              y: { xs: float },
-              space: { xs: anyURI }
-            }
-          },
-          Zoom: {
-            attributes: {
-              x: { xs: float },
-              space: { xs: anyURI }
-            }
-          }
-        },
-        MoveStatus: {
-          PanTilt: { xs: string },
-          Zoom: { xs: string }
-        },
-        Error: { xs: string },
-        UtcTime: { xs: dateTime }
-      }
-    }
-
-
-
     var date = new Date();
 
     this.ptzstatus = {
